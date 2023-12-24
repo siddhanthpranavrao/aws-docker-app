6a99fa53740c0203caf81246530d54254e4618a0
Siddhanth Pranav Rao <siddhanthprao@gmail.com>
2023-12-24 11:29:39 +0530

    Changes

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello Docker!!! 🐳🐳🐳'});
+    res.status(200).json({ message: 'Hello Docker BROOOO!!! 🐳🐳🐳'});
 });
 
 app.post('/', (req, res) => {
-    res.status(200).json({ message: 'Hello Docker!!! 🐳🐳🐳'});
+    res.status(200).json({ message: 'Hello Docker BROOOO!!! 🐳🐳🐳'});
 });
 
 app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));``
